fe0bf73a2436e3ff136604822506f25cf4c19baa
EgorZH <egor2001zhukov@gmail.com>
2023-05-03 21:04:17 +0300

    Add func main()

--- main.py
+++ main.py
@@ -16,3 +16,31 @@ def score_modulation(text):
     else:
         score = ""
     return score
+
+
+def main():
+    last_five = []
+    with open("operations.json", "r") as file:
+        operations = json.load(file)
+        operations.sort(
+            key=lambda x: datetime.strptime(x['date'], "%Y-%m-%dT%H:%M:%S.%f") if x.get('date') else datetime.min,
+            reverse=True)
+        for x in range(0, 5):
+            date = datetime.strftime(datetime.strptime(operations[x].get('date'),
+                                                       "%Y-%m-%dT%H:%M:%S.%f").date(), "%d.%m.%Y")
+            description = operations[x].get('description')
+            if operations[x].get("from"):
+                score_from = f'{score_modulation(operations[x].get("from"))} -> '
+            else:
+                score_from = ""
+            score_to = score_modulation(operations[x].get('to'))
+            bablishko = f"{operations[x].get('operationAmount').get('amount')} " \
+                        f"{operations[x].get('operationAmount').get('currency').get('name')}"
+            last_five.append(f"{date} {description}\n"
+                             f"{score_from}{score_to}\n"
+                             f"{bablishko}\n\n")
+        return "".join(last_five)
+
+
+if __name__ == '__main__':
+    print(main())
