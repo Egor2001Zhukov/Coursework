59517069bfb4e42720a112789594a4c77d9eefda
EgorZH <egor2001zhukov@gmail.com>
2023-05-04 11:52:11 +0300

    Reformat file

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import json
 from _datetime import datetime
 
 
+def load_jsonfile(filename: str):
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
 def score_modulation(text):
     if text:
         score_or_cart = text.split()
@@ -18,28 +23,37 @@ def score_modulation(text):
     return score
 
 
-def main():
+def last_five_operations(operations):
     last_five = []
-    with open("operations.json", "r") as file:
-        operations = json.load(file)
-        operations.sort(
-            key=lambda x: datetime.strptime(x['date'], "%Y-%m-%dT%H:%M:%S.%f") if x.get('date') else datetime.min,
-            reverse=True)
-        for x in range(0, 5):
-            date = datetime.strftime(datetime.strptime(operations[x].get('date'),
-                                                       "%Y-%m-%dT%H:%M:%S.%f").date(), "%d.%m.%Y")
-            description = operations[x].get('description')
-            if operations[x].get("from"):
-                score_from = f'{score_modulation(operations[x].get("from"))} -> '
-            else:
-                score_from = ""
-            score_to = score_modulation(operations[x].get('to'))
-            bablishko = f"{operations[x].get('operationAmount').get('amount')} " \
-                        f"{operations[x].get('operationAmount').get('currency').get('name')}"
-            last_five.append(f"{date} {description}\n"
-                             f"{score_from}{score_to}\n"
-                             f"{bablishko}\n\n")
-        return "".join(last_five)
+    operations.sort(
+        key=lambda y: datetime.strptime(y['date'], "%Y-%m-%dT%H:%M:%S.%f") if y.get('date') else datetime.min,
+        reverse=True)
+    for x in range(0, 5):
+        date = datetime.strftime(datetime.strptime(operations[x].get('date'),
+                                                   "%Y-%m-%dT%H:%M:%S.%f").date(), "%d.%m.%Y")
+
+        description = operations[x].get('description')
+
+        if operations[x].get("from"):
+            score_from = f'{score_modulation(operations[x].get("from"))} -> '
+        else:
+            score_from = ""
+
+        score_to = score_modulation(operations[x].get('to'))
+
+        bablishko = f"{operations[x].get('operationAmount').get('amount')} " \
+                    f"{operations[x].get('operationAmount').get('currency').get('name')}"
+
+        last_five.append(f"{date} {description}\n"
+                         f"{score_from}{score_to}\n"
+                         f"{bablishko}\n\n")
+    return "".join(last_five)
+
+
+def main():
+    operations = load_jsonfile("operations.json")
+    result = last_five_operations(operations)
+    return result
 
 
 if __name__ == '__main__':
